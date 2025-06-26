ad8a4904dea82d438ff4821075c2d167da953bab
VasylZarichnyi <vasylzarichnyi@gmail.com>
2025-06-26 23:39:34 +0300

    remove commented DragDropProvider

--- src/main.tsx
+++ src/main.tsx
@@ -5,14 +5,11 @@ import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux';
 import { store } from './store/index.ts';
-// import { DragDropProvider } from '@atlaskit/pragmatic-drag-and-drop-react';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <Provider store={store}>
-      {/* <DragDropProvider> */}
         <App />
-      {/* </DragDropProvider> */}
     </Provider>
   </StrictMode>,
 )
